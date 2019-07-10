81f7809286541afcfa4dfe859165d1ca60d4852d
Egor Frolov <adromaryn@gmail.com>
2019-07-10 22:55:18 +0300

    intervals tasks, start lists

---
+++ 2/list-operations.scm
@@ -0,0 +1,8 @@
+#lang sicp
+(define squares (list 1 4 9 16 25))
+(list-ref squares 3)
+(null? squares)
+(define odds (list 1 3 5 7))
+(length squares)
+(append squares odds)
+(reverse (list 1 4 9 16 25))
